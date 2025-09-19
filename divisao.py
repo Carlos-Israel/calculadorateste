9ed79b571b6cc783f7a80864965bebf1e7c18980
Carlos-Israel <carlosisrael979@gmail.com>
2025-09-18 23:04:10 -0300

    versao final

--- divisao.py
+++ divisao.py
@@ -1,6 +1,6 @@
 def divisao(A, B):
     if B == 0:
-        raise ValueError("Divisão por zero não é permitida.")
+        return ValueError("Divisão por zero não é permitida.")
     else:
         divisao = A / B
         return divisao
